fbc79b81b180afbf0798bf53ccdb975d680ec8a0
chenyuan <chen.yuan135@chinaredstar.com>
2019-08-09 18:14:39 +0800

    leanning

---
+++ Leanning-utils/src/main/java/com/rm13/util/snowflake/SnowflakeTest.java
@@ -0,0 +1,18 @@
+package com.rm13.util.snowflake;
+
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
+
+/**
+ * 雪花算法
+ * @author yuan.chen
+ * @email chen.yuan135@chinaredstar.com
+ * @Date 2019-08-08
+ */
+public class SnowflakeTest {
+
+    public static void main(String[] args){
+        Snowflake snowflake = IdUtil.createSnowflake(10, 11);
+        System.out.println(snowflake.nextId());
+    }
+}
--- Leanning-utils/src/main/java/com/rm13/util/snowflake/readme.md
+++ Leanning-utils/src/main/java/com/rm13/util/snowflake/readme.md
@@ -3,7 +3,18 @@ snowflake是Twitter开源的分布式ID生成算法，结果是一个long型的I
 
 ![snowflake](/Users/chenyuan/Documents/project/collect/Leanning/Leanning-utils/src/main/resources/img/snowflake.png)
 
+优点：全局唯一性，并发高，性能好，易恢复，易扩展
 
-- 0-1：符号为：默认为0
-- 2-42：
+缺点：收时钟回拨影响
+
+##
+1）第1位：符号位，0表示正数，1表示负数，发号器第一位默认为0
+
+2）第2-42位：时间戳，精确到毫秒
+
+3）第43-52位：机器ID（机房ID+服务器ID）
+
+4）第53-64位：序列号，自增长，支持同一个节点1ms可产生4096个ID
+
+
 
