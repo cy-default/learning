4907eb57d38beb7260701529d9de6240dcc76a63
chenyuan <chen.yuan135@chinaredstar.com>
2019-11-07 20:47:10 +0800

    leanning-springboot

---
+++ Leanning-springboot/src/main/java/com/rm13/springboot/LeanningSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.rm13.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanningSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanningSpringbootApplication.class, args);
+	}
+
+}
---
+++ Leanning-springboot/src/test/java/com/rm13/springboot/LeanningSpringbootApplicationTests.java
@@ -0,0 +1,8 @@
+package com.rm13.springboot;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanningSpringbootApplicationTests {
+
+}
