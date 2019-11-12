aa25e1284b07dc4198f04e8fad90a0d25ea7cb25
chenyuan <chen.yuan135@chinaredstar.com>
2019-11-12 20:33:31 +0800

    leanning

---
+++ Leanning-kafka/src/main/java/com/rm13/kafka/LeanningKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.rm13.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanningKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeanningKafkaApplication.class, args);
+    }
+
+}
---
+++ Leanning-kafka/src/test/java/com/rm13/kafka/LeanningKafkaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rm13.kafka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanningKafkaApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
