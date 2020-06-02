dc5ee2e2482b9f079730cd774cb68f6e07bc8f9e
chenyuan <chen.yuan@redstarclouds.com>
2020-06-02 19:57:56 +0800

    learning

---
+++ learning-wx/src/main/java/com/rm13/wx/LearningWxApplication.java
@@ -0,0 +1,13 @@
+package com.rm13.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningWxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningWxApplication.class, args);
+	}
+
+}
---
+++ learning-wx/src/test/java/com/rm13/wx/learningwx/LearningWxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm13.wx.learningwx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningWxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
