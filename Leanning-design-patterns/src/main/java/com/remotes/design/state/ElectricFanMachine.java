dd7c5020c2269cba29e014df061c66602906af98
chenyuan <chen.yuan@redstarclouds.com>
2020-07-23 20:35:25 +0800

    feature: 条件队列/AQS队列

---
+++ Leanning-design-patterns/src/main/java/com/remotes/design/state/ElectricFanMachine.java
@@ -0,0 +1,63 @@
+package com.remotes.design.state;
+
+/**
+ * 电扇状态机
+ *
+ * @author yuan.chen
+ * @email chen.yuan135@chinaredstar.com
+ * @Date 2020/7/6
+ */
+public class ElectricFanMachine {
+
+    private Integer windSpeed;
+    private IState state;
+
+    public ElectricFanMachine(IState state) {
+        this.state = state;
+    }
+
+    /**
+     * lowSpeed
+     */
+    public void lowSpeed() {
+        this.state.lowSpeed(this);
+    }
+
+    /**
+     * mediumSpeed
+     */
+    public void mediumSpeed() {
+        this.state.mediumSpeed(this);
+    }
+
+    /**
+     * highSpeed
+     */
+    public void highSpeed() {
+        this.state.highSpeed(this);
+    }
+
+    /**
+     * turningOff
+     */
+    public void turningOff() {
+        this.state.turningOff(this);
+    }
+
+
+    public void setState(IState state) {
+        this.state = state;
+    }
+
+    public Integer getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(Integer windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public IState getState() {
+        return state;
+    }
+}
