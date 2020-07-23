f77432125941edfa7ae9c36ef66fd6846a9fa229
chenyuan <chen.yuan@redstarclouds.com>
2020-07-23 20:33:14 +0800

    feature: 条件队列/AQS队列

---
+++ learning-wx/src/main/java/com/rm13/wx/config/WxMaProperties.java
@@ -0,0 +1,47 @@
+package com.rm13.wx.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+/**
+ * @author yuan.chen
+ * @email chen.yuan135@chinaredstar.com
+ * @Date 2020/6/2
+ */
+@Data
+@ConfigurationProperties(prefix = "wx.miniapp")
+public class WxMaProperties {
+
+    private List<Config> configs;
+
+    @Data
+    public static class Config {
+        /**
+         * 设置微信小程序的appid
+         */
+        private String appid;
+
+        /**
+         * 设置微信小程序的Secret
+         */
+        private String secret;
+
+        /**
+         * 设置微信小程序消息服务器配置的token
+         */
+        private String token;
+
+        /**
+         * 设置微信小程序消息服务器配置的EncodingAESKey
+         */
+        private String aesKey;
+
+        /**
+         * 消息格式，XML或者JSON
+         */
+        private String msgDataFormat;
+    }
+
+}
