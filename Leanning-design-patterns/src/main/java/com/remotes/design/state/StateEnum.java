a9ac61f357c2c6fc389122e9aee5c974feaa2234
chenyuan <chen.yuan@redstarclouds.com>
2020-07-23 20:37:48 +0800

    feature: 条件队列/AQS队列

---
+++ Leanning-design-patterns/src/main/java/com/remotes/design/state/StateEnum.java
@@ -0,0 +1,44 @@
+package com.remotes.design.state;
+
+import lombok.Data;
+import lombok.Getter;
+
+/**
+ * 状态机 状态枚举
+ *
+ * @author yuan.chen
+ * @email chen.yuan135@chinaredstar.com
+ * @Date 2020/7/6
+ */
+@Getter
+public enum StateEnum {
+    /**
+     * low speed
+     */
+    LOW_SPEED(1, 300, "low speed"),
+
+    /**
+     * medium speed
+     */
+    MEDIUM_SPEED(2, 600, "medium speed"),
+
+    /**
+     * high speed
+     */
+    HIGH_SPEED(3, 900, "high speed"),
+
+    /**
+     * turning off
+     */
+    TURNING_OFF(4, 0, "turning off");
+
+    private Integer code;
+    private Integer spreed;
+    private String desc;
+
+    StateEnum(Integer code, Integer spreed, String desc) {
+        this.code = code;
+        this.spreed = spreed;
+        this.desc = desc;
+    }
+}
---
+++ Leanning-design-patterns/src/main/java/com/remotes/design/state/StateTest.java
@@ -0,0 +1,27 @@
+package com.remotes.design.state;
+
+/**
+ * @author yuan.chen
+ * @email chen.yuan135@chinaredstar.com
+ * @Date 2020/7/6
+ */
+public class StateTest {
+
+    public static void main(String[] args) {
+
+        // 初始化关闭状态
+        final ElectricFanMachine electricFanMachine = new ElectricFanMachine(new TurningOffState());
+
+        // 切换低档
+        electricFanMachine.lowSpeed();
+
+        // 切换中档
+        electricFanMachine.mediumSpeed();
+
+        // 切换高档
+        electricFanMachine.highSpeed();
+
+        // 切换关闭
+        electricFanMachine.turningOff();
+    }
+}
