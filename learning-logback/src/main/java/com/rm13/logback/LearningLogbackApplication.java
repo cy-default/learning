04f8c04d7aee39bec09a3a68de0ef70173b4ac3a
chenyuan <chen.yuan@redstarclouds.com>
2021-01-05 18:18:42 +0800

    logback learning

---
+++ learning-logback/src/main/java/com/rm13/logback/LearningLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.rm13.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningLogbackApplication.class, args);
+    }
+
+}
---
+++ learning-logback/src/test/java/com/rm13/logback/LearningLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rm13.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
